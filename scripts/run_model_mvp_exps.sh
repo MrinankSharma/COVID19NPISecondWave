2fdb60084e4e13046a66e5eb7538b61fd82e3005
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2021-02-16 13:08:06 +0000

    MVp exps bugfix

--- scripts/run_model_mvp_exps.sh
+++ scripts/run_model_mvp_exps.sh
@@ -5,6 +5,6 @@ export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_thread
 for i in 0 1 2 3 4 5 6 7 8
 do
    echo "Running Experiment $i"
-   python scripts/model_mvp_exps.py --exp $i --num_chains 4 &
+   python scripts/model_mvp_exps.py --exp $i --num_chains 1 &
    sleep 15
 done
