a60356e105a7e936486849bfa30cd5029683d253
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2021-02-16 12:39:35 +0000

    MVp exps bugfix

--- scripts/run_model_mvp_exps.sh
+++ scripts/run_model_mvp_exps.sh
@@ -3,6 +3,6 @@
 for i in {0..8}
 do
    echo "Running Experiment $i"
-   python scripts/model_mvp_exps.sh --exp $i --num_chains 2 &
+   python scripts/model_mvp_exps.py --exp $i --num_chains 2 &
    sleep 5
 done
