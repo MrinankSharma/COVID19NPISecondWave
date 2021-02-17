2eba68a96eeeae1f5318155ddb7b08b4cdfae0d6
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2021-02-17 15:23:57 +0000

    rc candidate

--- epimodel/models/model_build_utils.py
+++ epimodel/models/model_build_utils.py
@@ -9,11 +9,11 @@ import numpyro.distributions as dist
 from epimodel.distributions import AsymmetricLaplace
 
 
-def create_intervention_prior(nCMs, intervention_prior=None):
+def sample_intervention_effects(nCMs, intervention_prior=None):
     if intervention_prior is None:
         intervention_prior = {
             "type": "asymmetric_laplace",
-            "scale": 55,
+            "scale": 30,
             "asymmetry": 0.5,
         }
 
@@ -44,37 +44,44 @@ def create_intervention_prior(nCMs, intervention_prior=None):
         )
     else:
         raise ValueError(
-            "Intervention effect prior must take a value in [trunc_normal, normal, asymmetric_laplace]"
+            "Intervention effect prior must take a value in [trunc_normal, normal, asymmetric_laplace, half_normal]"
         )
 
     return alpha_i
 
 
-def get_discrete_renewal_transition_from_projmat(gi_projmat):
-    """
-    Create discrete renewal transition function, used by `jax.lax.scan`
+def sample_basic_R(nRs, basic_r_prior=None):
+    if basic_r_prior is None:
+        basic_r_prior = {"mean": 1.1, "type": "trunc_normal", "variability": 0.3}
 
-    :param gi_projmat: gi projection matrix
-
-    :return: Discrete Renewal Transition function, with relevant GI parameters
-    """
-
-    def discrete_renewal_transition(infections, R_with_noise_tuple):
-        # infections is an nR x total_padding size array of infections in the previous
-        # total_padding days.
-        R, inf_noise = R_with_noise_tuple
-        new_infections = infections @ gi_projmat
-        new_infections = jax.ops.index_update(
-            new_infections,
-            jax.ops.index[:, -1],
-            jnp.multiply(new_infections[:, -1], R) + inf_noise,
+    if basic_r_prior["type"] == "trunc_normal":
+        basic_R = numpyro.sample(
+            "basic_R",
+            dist.TruncatedNormal(
+                low=0.1,
+                loc=basic_r_prior["mean"],
+                scale=basic_r_prior["variability"] * jnp.ones(nRs),
+            ),
         )
-        return new_infections, new_infections[:, -1]
+    else:
+        raise ValueError("Basic R prior type must be in [trunc_normal]")
 
-    return discrete_renewal_transition
+    return basic_R
 
 
-def get_discrete_renewal_transition(ep, type="optim"):
+def seed_infections(seeding_scale, nRs, nDs, seeding_padding, total_padding):
+    total_infections_placeholder = jnp.zeros((nRs, seeding_padding + nDs))
+    seeding = numpyro.sample("seeding", dist.LogNormal(jnp.zeros((nRs, 1)), 1.0))
+    init_infections = jnp.zeros((nRs, total_padding))
+    init_infections = jax.ops.index_add(
+        init_infections,
+        jax.ops.index[:, -seeding_padding:],
+        jnp.repeat(seeding ** seeding_scale, seeding_padding, axis=-1),
+    )
+    return init_infections, total_infections_placeholder
+
+
+def get_discrete_renewal_transition(ep, type="noiseless"):
     """
     Create discrete renewal transition function, used by `jax.lax.scan`
 
@@ -89,7 +96,9 @@ def get_discrete_renewal_transition(ep, type="optim"):
             # infections is an nR x total_padding size array of infections in the previous
             # total_padding days.
             R, inf_noise = R_with_noise_tuple
-            new_infections_t = jnp.multiply(R, infections @ ep.GI_flat_rev) + inf_noise
+            new_infections_t = jax.nn.softplus(
+                jnp.multiply(R, infections @ ep.GI_flat_rev) + inf_noise
+            )
             new_infections = infections
             new_infections = jax.ops.index_update(
                 new_infections, jax.ops.index[:, :-1], infections[:, 1:]
@@ -109,7 +118,7 @@ def get_discrete_renewal_transition(ep, type="optim"):
             new_infections = jax.ops.index_update(
                 new_infections,
                 jax.ops.index[:, -1],
-                jnp.multiply(new_infections[:, -1], R) + inf_noise,
+                jax.nn.softplus(jnp.multiply(new_infections[:, -1], R) + inf_noise),
             )
             return new_infections, new_infections[:, -1]
 
@@ -134,32 +143,27 @@ def get_discrete_renewal_transition(ep, type="optim"):
     return discrete_renewal_transition
 
 
-def observe_cases_deaths(data, expected_cases, expected_deaths):
-    """
-    Observation model
+def get_output_delay_transition(seeding_padding, data):
+    def output_delay_transition(loop_carry, scan_slice):
+        # this scan function scans over local areas, using their country specific delay, rather than over days
+        # therefore the input functions are ** not ** transposed.
+        # Also, we don't need a loop carry, so we just return 0 and ignore the loop carry!
+        (
+            future_cases,
+            future_deaths,
+            country_cases_delay,
+            country_deaths_delay,
+        ) = scan_slice
+        expected_cases = jax.scipy.signal.convolve(
+            future_cases, country_cases_delay, mode="full"
+        )[seeding_padding : data.nDs + seeding_padding]
+        expected_deaths = jax.scipy.signal.convolve(
+            future_deaths, country_deaths_delay, mode="full"
+        )[seeding_padding : data.nDs + seeding_padding]
 
-    :param data: PreprocessedData Object
-    :param expected_cases: Expected Cases - nRs x nDs array
-    :param expected_deaths: Expected Deaths - nRs x nDs array
-    """
-    psi_cases = numpyro.sample("psi_cases", dist.HalfNormal(5))
-    psi_deaths = numpyro.sample("psi_deaths", dist.HalfNormal(5))
+        return 0.0, (expected_cases, expected_deaths)
 
-    with numpyro.handlers.mask(mask=jnp.logical_not(data.new_cases.mask)):
-        observed_cases = numpyro.sample(
-            "observed_cases",
-            dist.GammaPoisson(concentration=psi_cases, rate=psi_cases / expected_cases),
-            obs=data.new_cases.data,
-        )
-
-    with numpyro.handlers.mask(mask=jnp.logical_not(data.new_deaths.mask)):
-        observed_deaths = numpyro.sample(
-            "observed_deaths",
-            dist.GammaPoisson(
-                concentration=psi_deaths, rate=psi_deaths / expected_deaths
-            ),
-            obs=data.new_deaths.data,
-        )
+    return output_delay_transition
 
 
 def setup_dr_infection_model(data, ep):
@@ -193,48 +197,6 @@ def setup_dr_infection_model(data, ep):
     )
 
     return init_infections, total_infections, infection_noise, seeding_padding
-
-
-def get_output_delay_transition(seeding_padding, data):
-    def output_delay_transition(loop_carry, scan_slice):
-        # this scan function scans over local areas, using their country specific delay, rather than over days
-        # we don't need a loop carry, so we just return 0 and ignore the loop carry!
-        (
-            future_cases,
-            future_deaths,
-            country_cases_delay,
-            country_deaths_delay,
-        ) = scan_slice
-        expected_cases = jnp.convolve(future_cases, country_cases_delay, mode="full")[
-            seeding_padding : data.nDs + seeding_padding
-        ]
-        expected_deaths = jnp.convolve(
-            future_deaths, country_deaths_delay, mode="full"
-        )[seeding_padding : data.nDs + seeding_padding]
-
-        return 0.0, (expected_cases, expected_deaths)
-
-    return output_delay_transition
-
-
-def create_basic_R_prior(nRs, basic_r_prior=None):
-    if basic_r_prior is None:
-        basic_r_prior = {"mean": 1.1, "type": "trunc_normal", "variability": 0.5}
-
-    if basic_r_prior["type"] == "trunc_normal":
-        basic_R_variability = numpyro.sample(
-            "basic_R_variability", dist.HalfNormal(0.5)
-        )
-        basic_R = numpyro.sample(
-            "basic_R",
-            dist.TruncatedNormal(
-                low=0.1, loc=1.1 * jnp.ones(nRs), scale=basic_R_variability
-            ),
-        )
-    else:
-        raise ValueError("Basic R prior type must be in [trunc_normal]")
-
-    return basic_R
 
 
 def create_noisescale_prior(varname, noisescale_prior, type="r_walk"):
