eb49942dac60bc08dd15608fdb37838c1d5b6297
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2021-02-24 16:43:12 +0000

    config changes

--- epimodel/script_utils.py
+++ epimodel/script_utils.py
@@ -8,12 +8,14 @@ import os
 import numpy as np
 import yaml
 
-from epimodel.models.release_candidate_model_v1 import rc_model_1a
+from epimodel.models.release_candidate_model_v1 import rc_model_1a, rc_model_2a
 
 
 def get_model_func_from_str(model_type_str):
     if model_type_str == "rc_1a":
         return rc_model_1a
+    if model_type_str == "rc_2a":
+        return rc_model_2a
 
 
 def get_target_accept_from_model_str(model_type_str):
